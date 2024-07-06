a1348641bbfd3e6f7ef20768c4fc025b241f3f91
Bhanushreek <155749109+Bhanushreek@users.noreply.github.com>
2024-07-06 14:24:03 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"new strict" // treat all JS code as newer version
+
+// alert( 3 + 3 ) we are using nodejs not browser
+
+let name = "Bhanu"
+let age = 18
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+//symbols => unique
+
+
+//object
+
+console.log(typeof undefined) //output :- undefined
+console.log(typeof null) // output :- object
+
+//ecma specifications, mdn documentation
