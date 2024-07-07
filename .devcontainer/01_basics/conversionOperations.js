3d09d5beacca7b2cb709137495c27a0676ee4643
Bhanushreek <155749109+Bhanushreek@users.noreply.github.com>
2024-07-07 05:50:14 +0000

    Conversion of datatypes

---
+++ .devcontainer/01_basics/conversionOperations.js
@@ -0,0 +1,28 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Bhanu"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+//NaN => Not a Number
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Bhanu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
