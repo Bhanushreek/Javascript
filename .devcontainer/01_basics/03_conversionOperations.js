fb23eb584d27977a3e29ac1c686ae54e49062679
Bhanushreek <155749109+Bhanushreek@users.noreply.github.com>
2024-07-07 06:41:37 +0000

    conversions done

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,55 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Bhanu"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+//NaN => Not a Number
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Bhanu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******** OPERATIONS ********* //
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = " bhanu"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log(1 + "2")  NOT PREFFERABLE
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter
+// gameCounter++
+console.log(gameCounter);
+
+// checkoout precedence from js mdn
