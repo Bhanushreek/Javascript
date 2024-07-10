4b66df334f2837d9f79d730c1ce7b3181870d771
Bhanushreek <155749109+Bhanushreek@users.noreply.github.com>
2024-07-10 07:34:39 +0000

    string datatype

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "bhanu "
+const repoCount = 50
+
+// console.log(name + repoCount + " Value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`)
+
+const gameName = new String(`bhanushree`)
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('s'))
