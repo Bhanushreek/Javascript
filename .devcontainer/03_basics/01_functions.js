19af0f00cfca8d1211f44a0ff62b2136cdec47e4
Bhanushreek <155749109+Bhanushreek@users.noreply.github.com>
2024-07-12 15:09:37 +0000

    functions done!

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -37,4 +37,39 @@ function loginUserMessage(username) { // to pass default value (username = "sam"
 }
 
 // console.log(loginUserMessage("Bhanu"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartPrice(...num1) {
+    return num1;
+}
+
+// console.log(calculateCartPrice(200, 300, 500)); // => gives values as an array
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1;
+}
+
+// console.log(calculateCartPrice(200, 300, 500, 2000));
+
+const user = {
+    username: "bhanu",
+    price: 199 
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([1, 2, 3, 4]));
